7f5f7e79a2f793907f5ca2385b74073486a04038
Shrushtiwarke <shrushtiwarke2002@gmail.com>
2025-09-17 15:18:39 +0530

    my first stage code

---
+++ b.py
@@ -0,0 +1,33 @@
+#with try except block
+
+try:
+    numerator = 10
+    denominator = "a"
+    result = numerator/denominator  #  math operation
+
+    print(result)
+except:
+    print("Error: Denominator cannot be 0.")
+
+# Output: Error: Denominator cannot be 0.
+
+
+print("one332323")
+print("two3232")
+print("three3322")
+print("fou45354r")
+print("five54354")
+print("six4543543")
+print("seven45345")
+print("eight54543")
+print("nine5454")
+print("ten54543")
+
+
+
+
+
+
+
+
+
---
+++ c.py
@@ -0,0 +1,25 @@
+# #❌ Example 1: Without self (Wrong way)
+# class Student:
+#     def set_data(name, age):   # no self
+#         name = name
+#         age = age
+#
+#     def display():
+#         print(f"My name is {name} and I am {age} years old.")
+#
+# s1 = Student()
+# s1.set_data("Ajay", 21)   # ❌ Error
+# s1.display()
+#
+# s2= Student()
+
+
+
+
+
+
+
+
+
+
+
